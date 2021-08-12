398ea6ec8c29814fabc2abfa34b366c3042c26c5
asusGuy94 <asusGuy94.1@gmail.com>
2021-08-12 09:25:06 +0200

    Added optional env-variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,6 +1,6 @@
 export default {
-	host: '',
-	user: '',
-	password: '',
-	database: ''
+	host: process.env.DB_HOST || '',
+	user: process.env.DB_USER || '',
+	password: process.env.DB_PASS || '',
+	database: process.env.DB || ''
 }
