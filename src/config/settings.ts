d4d5d2c0a01d86539c8aaa9435a2b7154279763f
asusGuy94 <asusGuy94.1@gmail.com>
2021-10-24 14:40:26 +0200

    Changed 8k to 6k
    * 6k is actually used, while 8k is seldom used

--- src/config/settings.ts
+++ src/config/settings.ts
@@ -1,4 +1,3 @@
 export default {
-	// no content is at 8k, so adding more would just slow down the app
-	qualities: [4320, 2160, 1440, 1080, 720, 480, 360]
+	qualities: [3132, 2160, 1440, 1080, 720, 480, 360]
 }
